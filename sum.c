e42e4b93824aecc5b3d9f44474a95db3e257a7aa
Sahabuddin Shakil <mdshakil8796@gmail.com>
2021-02-12 03:45:29 +0600

    basic c

---
+++ sum.c
@@ -0,0 +1,11 @@
+# include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter number of a:\n");
+    scanf("%d",&a);
+    printf("Enter number of b:\n");
+    scanf("%d",&b);
+    c= a+b;
+    printf("%d + %d = %d",a,b,c);
+}
---
+++ variable.c
@@ -0,0 +1,22 @@
+// take input character
+# include<stdio.h>
+int main()
+{
+  char c;
+  printf("Enter the character:\n");
+  scanf("%c",&c); 
+  printf("%c",c);
+  return 0;
+}
+
+
+// take input int
+# include<stdio.h>
+int main()
+{
+  int a ;
+  printf("Enter the int:\n");
+  scanf("%d",&a); 
+  printf("%d",a);
+  return 0;
+}
