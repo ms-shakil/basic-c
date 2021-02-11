190b51cab1a2cd286d129d3fab755d8ff4578cac
Sahabuddin Shakil <mdshakil8796@gmail.com>
2021-02-12 03:58:23 +0600

    function example

---
+++ function_example.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+int squre(int a){
+    int temp;
+    temp = a*a;
+    return temp;
+}
+int main()
+{
+int a = 5, result;
+result = squre(a);
+printf("your result is: %d",result);
+}
