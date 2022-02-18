b77e84f6a7f112af2a56966945fa97fa3e51fd21
ms-shakil <mdshakil8796@gmail.com>
2022-02-19 01:17:35 +0600

    about Some builtin function

---
+++ Math_Function.c
@@ -0,0 +1,14 @@
+// Some  Builtin Funciton
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main(){
+    int num = abs(56);
+    double num2 = sqrt(25);
+    double num3 = pow(5,3);
+    printf(" absulate number %d \n",num);
+    printf(" squreroot number%lf \n",num2);
+    printf("Power number %lf",num3);
+}
